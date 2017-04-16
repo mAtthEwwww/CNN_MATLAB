79b4a39a91edec7cb43cfb6313d8f09dae21e99b
mAtthEwwww <mattwaizuen@outlook.com>
2017-04-16 15:17:22 +0800

    modified

--- MNIST_main2.m
+++ MNIST_main2.m
@@ -27,8 +27,8 @@ tr_config.half_life = 4;
 tr_config.momentum = 0;
 tr_config.weight_decay = 0.0001;
 tr_config.batch_size = 50;
-tr_config.validate_interval = 200;
-tr_config.max_epochs = 3;
+tr_config.validate_interval = 600;
+tr_config.max_epochs = 6;
 tr_config.cost_function = 'cross_entropy';
 tr_config.threshold = 0.002;
 
@@ -43,7 +43,7 @@ CNN{ l }.output = input_channel;
 
 l = l + 1;
 CNN{ l }.type = 'convolution';
-CNN{ l }.weight.shape = [5, 5];
+CNN{ l }.weight.shape = [3, 3];
 CNN{ l }.weight.filler.type = 'xavier';
 CNN{ l }.weight.learning_rate = 1;
 CNN{ l }.bias.option = false;
@@ -68,7 +68,7 @@ CNN{ l }.BN_decay = 0.99;
 
 l = l + 1;
 CNN{ l }.type = 'convolution';
-CNN{ l }.weight.shape = [5, 5];
+CNN{ l }.weight.shape = [3, 3];
 CNN{ l }.weight.filler.type = 'xavier';
 CNN{ l }.weight.learning_rate = 1;
 CNN{ l }.bias.option = false;
@@ -92,7 +92,7 @@ CNN{ l }.activation = 'relu';
 
 l = l + 1;
 CNN{ l }.type = 'convolution';
-CNN{ l }.weight.shape = [5, 5];
+CNN{ l }.weight.shape = [3, 3];
 CNN{ l }.weight.filler.type = 'xavier';
 CNN{ l }.weight.learning_rate = 1;
 CNN{ l }.bias.option = false;
@@ -160,7 +160,7 @@ for l = 2 : length( CNN )
         struct_str = sprintf( '%slayer %i   type: activation             %s\r\n' , struct_str, l, CNN{l}.activation );
     end
 end
-run_str = sprintf('Accuracy %.2f%%   cost %f   time %.1fs   epochs %i   learning rate %.7f   batchsize %i   momentum %.1f   half life %i   activation %s   weight filler %s', accuracy * 100, CNN{1}.cost, train_time, CNN{1}.epochs, tr_config.learning_rate, tr_config.batch_size, tr_config.momentum, tr_config.half_life, activation, weight_filler.type);
+run_str = sprintf('Accuracy %.2f%%   cost %f   time %.1fs   epochs %i   learning rate %.7f   batchsize %i   momentum %.1f   half life %i', accuracy * 100, CNN{1}.cost, train_time, CNN{1}.epochs, tr_config.learning_rate, tr_config.batch_size, tr_config.momentum, tr_config.half_life);
 log_str = sprintf( '%s\r\n%s' , run_str , struct_str );                 
 
 % output the running log
--- MNIST_main_RES.m
+++ MNIST_main_RES.m
@@ -23,7 +23,7 @@ clear train
 clear test
 
 tr_config.learning_rate = 0.1;
-tr_config.half_life = 4;
+tr_config.half_life = 5;
 tr_config.momentum = 0;
 tr_config.weight_decay = 0.0001;
 tr_config.batch_size = 50;
@@ -81,7 +81,7 @@ CNN{ l }.weight.learning_rate = 1;
 CNN{ l }.bias.learning_rate = 2;
 CNN{ l }.dropout.option = true;
 CNN{ l }.dropout.rate = 0.5;
-CNN{ l }.output = 200;
+CNN{ l }.output = 80;
 
 l = l + 1;
 CNN{ l }.type = 'activation';
@@ -124,7 +124,7 @@ for l = 2 : length( CNN )
         struct_str = sprintf( '%slayer %i   type: activation             %s\r\n' , struct_str, l, CNN{l}.activation );
     end
 end
-run_str = sprintf('Accuracy %.2f%%   cost %f   time %.1fs   epochs %i   learning rate %.7f   batchsize %i   momentum %.1f   half life %i   activation %s   weight filler %s', accuracy * 100, CNN{1}.cost, train_time, CNN{1}.epochs, tr_config.learning_rate, tr_config.batch_size, tr_config.momentum, tr_config.half_life, activation, weight_filler.type);
+run_str = sprintf('Accuracy %.2f%%   cost %f   time %.1fs   epochs %i   learning rate %.7f   batchsize %i   momentum %.1f   half life %i   weight filler %s', accuracy * 100, CNN{1}.cost, train_time, CNN{1}.epochs, tr_config.learning_rate, tr_config.batch_size, tr_config.momentum, tr_config.half_life, weight_filler.type);
 log_str = sprintf( '%s\r\n%s' , run_str , struct_str );                 
 
 % output the running log
